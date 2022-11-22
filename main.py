44472a88c7c8ba441f7a591a4b177fc31efac59c
Nguyễn Như Quang <quangnguyennhu@vccorp.vn>
2022-11-22 08:38:35 +0700

    a

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template, make_response
 import requests
-from flask_cors import CORS
 from flask_cors import cross_origin
 
 app = Flask(__name__)
-# ok
-# CORS(app, resources={r"*": {"origins": "*"}})
 
 @app.route("/plugin-vod-token")
 @cross_origin(supports_credentials=True,methods="get")
@@ -26,5 +23,5 @@ def token():
 
 @app.route("/")
 def home():
-    return render_template("traitim.html")
+    return render_template("cms.html")
 
