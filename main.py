1f831c37b0d4dce4048a0f93e9318fd1bbe5d1ed
Nguyễn Như Quang <quangnguyennhu@vccorp.vn>
2022-11-22 08:55:03 +0700

    Format code

--- main.py
+++ main.py
@@ -4,23 +4,47 @@ from flask_cors import cross_origin
 
 app = Flask(__name__)
 
+# Api lấy token cho plugin
 @app.route("/plugin-vod-token")
 @cross_origin(supports_credentials=True,methods="get")
 def token():
+    # Khởi tạo tham số
+    username = "" # Nhập username
+    account_suffix = "" # Nhập Account Suffix
+    code = "" # Nhập code
+    scope = "" # Nhập scope
     url = "https://vodbizflyapi.cnnd.vn/oauth/token"
-    payload = {'username': "quangnnvod_bizflyvod-quangnnvod-141e7",
+
+    # Tạo payload
+    payload = {'username': f"{username}{account_suffix}",
                'grant_type': 'authorization_code',
-               'code': '4dmilvwp3u',
-               'scope': '497318801742135296'}
+               'code': code,
+               'scope': scope}
     headers = {
         'Content-Type': 'application/x-www-form-urlencoded'
     }
+
+    # Gọi request lấy token
     data = requests.post(url, headers=headers, data=payload)
-    response = make_response(data.json()["message"]["token"], 200)
+
+    # Xử lý response
+    """
+    Dữ liệu response trả về có dạng:
+    {
+        "message":{
+            "token":"epBJqhqG1f7ObEks670-4NUX9X4QemxldFR3Hw...",
+            "expires":"2m"
+        },
+        "status":1
+    }
+    Cần lấy trường token và trả ra
+    """
+    token = data.json()["message"]["token"]
+    response = make_response(token, 200)
 
     return response
 
-
+# Demo CMS.html
 @app.route("/")
 def home():
     return render_template("cms.html")
